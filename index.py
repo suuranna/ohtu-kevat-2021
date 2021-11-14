69da59835c7092322f305714348731d8634d69ca
Anna Suuronen <annasuuronen@icloud.com>
2021-11-15 01:44:27 +0200

    muttos mainissa

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan nyt ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
